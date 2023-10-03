f3fa57831f8b4c02cf1ffea0d2b0fb015b049b2e
Syed Raza Haider <raxa.hai@gmail.com>
2023-10-04 04:03:05 +0500

    ✨ feat(app): structured config
    Structuring invloving initialization of routes as well as listening to exception handler at the root of the project

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware import Middleware
+import routes.product as product_router
+import routes.home as home_router
+from config.properties import config
+from exceptions.base import CustomException
 
-app = FastAPI(title="Forsit backend",
-              description="This is a test by Forsit", version="0.0.1")
+
+def init_routers(app: FastAPI) -> None:
+    app.include_router(product_router.router)
+    app.include_router(home_router.router)
 
 
-@app.get('/', status_code=200)
-def home():
-    return "This is home page"
+def init_listeners(app: FastAPI) -> None:
+    # Exception handler
+    @app.exception_handler(CustomException)
+    async def custom_exception_handler(request: Request, exception: CustomException):
+        return JSONResponse(
+            status_code=exception.code,
+            content={"error_code": exception.error_code,
+                     "error_message": exception.message},
+        )
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(title="Forsit backend",
+                  description="This is a test by Forsit", version="0.0.1", debug=config.DEBUG)
+    init_listeners(app=app)
+    init_routers(app=app)
+    return app
+
+
+app: FastAPI = create_app()
